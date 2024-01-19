153b6fe7437b9bc65b579d8ac104fcd4336fdcf9
AnthonyFerron <anthony.ferron74@gmail.com>
2024-01-19 16:38:42 +0100

    aa

--- C_projects/my_itoa.c
+++ C_projects/my_itoa.c
@@ -2,6 +2,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int my_strlen(char *str) {
+    int i = 0;
+    while(str[i] != '\0')
+        i++;
+    return(i - 1);
+}
+
+
 char *my_itoa(long long int nbr)
 {
     int i = 0;
@@ -16,22 +24,34 @@ char *my_itoa(long long int nbr)
     if(nbr < 0)
     {
         str[i] = '-';
+        nbr = -nbr;
         i++;
     }
     while(nbr > 0)
     {
         j = nbr % 10;
         nbr = nbr / 10;
-        if(nbr > 0)
-            str[i] = my_itoa(nbr);
-        i++;
         str[i] = '0' + j;
         i++;
+    }
+    
+    int start = 0;
+    if(str[0] == '-')
+        start = 1;
+    int end = my_strlen(str);
+    char temp;
+    while(start < end)
+    {
+        temp = str[end];
+        str[end] = str[start];
+        str[start] = temp;
+        start++;
+        end--;
     }
     return(str);
 }
 
 int main(void)
 {
-    printf("%s", my_itoa(123456789));
+    printf("%s", my_itoa(-123456789));
 }
