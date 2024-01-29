097fb2db11d54de1d67e60f76b8670b7d4c79727
AnthonyFerron <anthony.ferron74@gmail.com>
2024-01-29 14:47:09 +0100

    aa

--- 42_projets/C02/ex10/my_strlcpy.c
+++ 42_projets/C02/ex10/my_strlcpy.c
@@ -2,5 +2,25 @@
 
 unsigned int my_strlcpy(char *dest, char *src, unsigned int size)
 {
-     
+    int i = 0;
+    int j = 0;
+    while(dest[i] != '\0' && i < size) {
+        i++;
+    }
+    while(i < size && src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    // printf("%s\n", dest);
+    return(i);
 }
+
+// int main(void) {
+//     char dest[12] = "Hello ";
+//     char *src = "World";
+//     printf("%d", my_strlcpy(dest, src, 1));
+ 
+//     return 0;
+// }
